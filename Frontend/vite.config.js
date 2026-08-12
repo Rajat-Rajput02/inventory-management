e798e593730f27db45ebc2f93de265c8b4496d5a
Rajat-Rajput02 <rajatkumar022003@gmail.com>
2026-08-13 00:20:44 +0530

    fixed errors

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1600, // Raises warning threshold
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Splits third-party packages into separate files
+          }
+        },
+      },
+    },
+  },
+});
